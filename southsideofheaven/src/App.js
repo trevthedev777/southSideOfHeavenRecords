5fd6f1cfc195df25145e7ab744f77d28eedcb86b
Trevor Lehmann <trevor@trevorlehmann.com>
2023-03-14 22:18:05 +0000

    Update landing page compinent

--- southsideofheaven/src/App.js
+++ southsideofheaven/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import LandingPage from '../components/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
       </header>
     </div>
   );
